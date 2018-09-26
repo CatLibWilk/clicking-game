e47eac664e6ae114dacd47e6be213c2a37f2f279
Andy Wilk <andrewdwilk@gmail.com>
2018-09-26 18:28:57 -0400

    fixed to take image titles out

--- click-game/click-game/src/components/Card.js
+++ click-game/click-game/src/components/Card.js
@@ -4,7 +4,7 @@ import React from "react";
 const Card = ({id, name, image, onClick}) => (
 
     <div className="col-3 float-left mb-4 img-div" key={id} name={name}>
-    <p>{name}</p>
+
         <img className="img" src={image} onClick={() => onClick(this.id)} alt=""></img>
     </div>
 )
